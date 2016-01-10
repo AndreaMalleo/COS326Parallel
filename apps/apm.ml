678b96de72471aba4b127422d001d6cb44884291
mlea13 <mlea@princeton.edu>
2016-01-10 16:33:21 -0500

    part 2

--- apps/apm.ml
+++ apps/apm.ml
@@ -83,15 +83,82 @@ let print_matches (n : string) ((p, ps) : profile * (float * profile) list) : un
 
 
 
-module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)
-
+(*module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)*)
+module PSeq = Sequence.ListSeq
+		
 (* apm computes the potential love of your life.  The filename of a file
  * containing candidate profiles is in location 0 of the given array.
  * The number of matches desired is in location 1.  The first and last name
  * of the (human) client are in location 2 and 3, respectively.  The client's
  * profile must be in the profiles file.  Results are output to stdout. *)
-let matchme (args : string array) : unit = failwith "implement me"
 
+(*make parrallel...?*)
+let read_file filename =
+  let f = open_in filename in
+  let rec next accum =
+    match (try Some (input_line f) with End_of_file -> None) with
+    | None -> accum
+    | Some line -> next ((convert line) :: accum)
+  in
+  let profs = next [] in
+  close_in f;
+  profs
+;;
+
+let filter (l: 'a PSeq.t) (f: 'a -> bool): 'a PSeq.t =
+  let l_array = PSeq.array_of_seq l in
+  let bits = PSeq.map (fun x -> if f x then 1 else 0) l in
+  let bits_array = PSeq.array_of_seq bits in
+  let length = PSeq.reduce (fun sum x -> sum + x) 0 bits in
+  let bitsum = PSeq.scan (fun prev curr -> prev + curr) 0 bits in
+  let bitsum_array = PSeq.array_of_seq bitsum in
+  let filteredArray = Array.make length (PSeq.nth l 0) in
+  PSeq.tabulate (fun i ->
+		 if bits_array.(i) == 0 then i
+		 else (Array.set filteredArray (bitsum_array.(i)-1) l_array.(i); i))
+		(PSeq.length l);
+  PSeq.seq_of_array filteredArray
+;;
+
+let update (results: (int * float * profile) array)
+	   ((score, p): float * profile) (count: int): unit =
+  let old_results = PSeq.seq_of_array results in
+  let (index, _, _) = PSeq.reduce (fun (curr_i, _, _) (i, s, _) ->
+				   if curr_i = -1 then
+				     if (score > s) then (i, 0.0, p)
+				     else (curr_i, 0.0, p)
+				   else (curr_i, 0.0, p)) (-1, 0.0, p) old_results in
+  if index != -1 then
+    (PSeq.tabulate (fun i -> if i < index then i
+			    else if i = index then
+			      (Array.set results i (i, score, p); i)
+			     else (Array.set results i (PSeq.array_of_seq old_results).(i-1); i))
+		   count; ())
+  else () (*do nothing*)
+  
+let matchme (args : string array) : unit =
+  let match_count = int_of_string args.(1) in
+  let profs = read_file (args.(0)) in
+  let prof_seq = PSeq.seq_of_array (Array.of_list profs) in
+  let curr_profile = PSeq.nth (filter prof_seq (fun p ->
+						Printf.printf "%s%s" p.firstname args.(2);p.firstname = args.(2)
+							 && p.lastname = args.(3))) 0 in
+  Printf.printf "here";
+  let other_profs = filter prof_seq (fun p -> p.firstname != args.(2)
+					      || p.lastname != args.(3)) in
+  let score_seq = PSeq.map (*!! need to change score*)
+		    (fun p -> (1.0, p)) other_profs in
+  let results = Array.init match_count (fun i -> (i, 0.0, curr_profile)) in
+  PSeq.map (fun r -> update results r match_count) score_seq;
+  let processed_results = PSeq.map (fun (i, s, p) -> (s, p)) (PSeq.seq_of_array results) in
+  let final_results = Array.to_list (PSeq.array_of_seq
+				       (filter processed_results (fun (s, p) -> p != curr_profile))) in
+  print_matches args.(1) (curr_profile, final_results)
+;;
+
+
+
+						    
 
 
 
--- apps/inverted_index.ml
+++ apps/inverted_index.ml
@@ -1,10 +1,53 @@
 open Future
-
-module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)
-
+(*module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)*)
+module PSeq = Sequence.ListSeq
 
 (* inverted_index computes an inverted index for the contents of
  * a given file. The filename is the given string.
  * The results are output to stdout. *)
-let mkindex (args : string ) : unit = failwith "implement me"
-	
+open Sequence
+open Util
+       
+(*	
+let doc_get_words (doc: (string * string PSeq.t) PSeq.t) =
+  (*or should it be 0*)
+  let words = snd (PSeq.nth doc 0) in
+  PSeq.map (fun w -> PSeq.singleton (w, PSeq.empty)) words
+;;
+  
+let doc_get_id (doc: (string * string PSeq.t) PSeq.t) =
+  (*or should it be 0*)
+  fst (PSeq.nth doc 0)
+;;
+
+let docs_to_word_list (doc: document): (string * string PSeq.t) =
+   PSeq.seq_of_array (Array.of_list (split_words doc.contents))
+;;
+let find (list: 'a PSeq.t) (elt: 'a): bool =
+  match (PSeq.map_reduce (fun x -> Some x)
+			(fun result opt ->
+			 match opt with
+			 | Some y -> if elt = y then
+				       Some y
+				     else result
+			 | None -> failwith "should not reach") None list) with
+  | Some _ -> true
+  | None -> false
+;;
+  
+let reduce_docs (word_list: string PSeq.t) (doc_list: string PSeq.t): string PSeq.t =
+  PSeq.reduce (fun curr_word_list word ->
+	       if find curr_word_list word then
+		 curr_word_list
+	       else
+		 PSeq.cons curr_word_list word) word_list doc_list
+ *)
+let mkindex (args : string ) : unit =
+()
+(*  let docs = load_documents args in (*should we use map reduce to compute these?*)
+  let doc_seq = PSeq.seq_of_array (Array.of_list docs) in
+  let list_of_words = PSeq.map_reduce docs_to_word_list reduce_docs PSeq.empty docs_seq in
+  let results = PSeq.map (fun a -> (fst a, Array.to_list PSeq.array_of_seq (snd a))) raw_results in
+  print_reduce_results (Array.to_list PSeq.array_of_seq results)
+ *);;
+
